48e5b38bdffea03be9bb66677e460e798a186fe5
iamraviprakash <raviprakash.coder@gmail.com>
2020-04-06 10:38:41 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Detector } from "react-detect-offline";
+import gql from 'graphql-tag';
 import './App.css';
+import { useApolloClient } from "@apollo/react-hooks";
+
+const USER_QUERY = gql`
+  query ($id: ID!){
+    user(id: $id) {
+      id
+      name
+    }
+  }
+`;
+
+const UserList = ({result: {loading, error, data}}) => {
+  if (loading) return <div>Fetching...</div>;
+  if (error) return <div>Error</div>;
+  if (data.user) {
+    return (
+      <div>
+        <div>ID: {data.user.id} Name: {data.user.name}</div>
+      </div>
+    )
+  } else {
+    return <div>User Not Found</div>
+  }
+}
+
 
 function App() {
+
+  const client = useApolloClient();
+
+  const [result, setResult] = useState({
+    loading: true,
+    error: null,
+    data: null
+  });
+
+  const [formEditState, setFormEditState] = useState(true)
+
+  const handleConnectivityChange = (online) => {
+    console.log(online);
+
+    if (online) {
+      setFormEditState(false)
+    } else {
+      setFormEditState(true)
+    }
+  }
+
+
+  const handleInput = async (id) => {
+    const queryResult = await client.query({
+      query: USER_QUERY,
+      variables: { id: id }
+    });
+    setResult(queryResult);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Detector
+        onChange={handleConnectivityChange}
+        render={({ online }) => {
+          console.log({"formEditState": formEditState});
+          return (
+            <div className={online ? "online" : "offline"}>
+              You are currently {online ? "online" : "offline"}
+            </div>
+          )
+        }}
+      />
+
+      {/* Form */}
+      <fieldset className="form" disabled={formEditState}>
+        <form>
+          <input
+            placeholder="What needs to be done?"
+            onChange={e => (handleInput(e.target.value))}
+          />
+        </form>
+      </fieldset>
+      
+
+      {/* Results */}
+      <div className="user-list">
+        <UserList result={result} />
+      </div>
     </div>
   );
 }
