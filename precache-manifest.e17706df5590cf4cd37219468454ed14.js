0fc0674d2b2f6fee8b3dfa3b75918cd9229e5729
iamraviprakash <raviprakash.coder@gmail.com>
2020-04-06 11:06:53 +0530

    Updates

---
+++ precache-manifest.e17706df5590cf4cd37219468454ed14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e76c4a6000bf9b6d94c87148931a90f",
+    "url": "/network-management/index.html"
+  },
+  {
+    "revision": "3e02954e438185e049d7",
+    "url": "/network-management/static/css/main.e5817fe5.chunk.css"
+  },
+  {
+    "revision": "5331cd27248633d27bf9",
+    "url": "/network-management/static/js/2.89a1cdb3.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/network-management/static/js/2.89a1cdb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e02954e438185e049d7",
+    "url": "/network-management/static/js/main.c573f738.chunk.js"
+  },
+  {
+    "revision": "ffbd2bb8849d48e32c11",
+    "url": "/network-management/static/js/runtime-main.12728b9f.js"
+  }
+]);
