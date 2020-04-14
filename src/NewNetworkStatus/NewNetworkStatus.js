763576a56055f5c5a77786b7cbd750a5601af77d
iamraviprakash <raviprakash.coder@gmail.com>
2020-04-14 11:58:18 +0530

    Use PureComponent

--- src/NewNetworkStatus/NewNetworkStatus.js
+++ src/NewNetworkStatus/NewNetworkStatus.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import './NewNetworkStatus.css';
 import { Detector } from 'react-detect-offline';
 
-class NewNetworkStatus extends Component {
+class NewNetworkStatus extends PureComponent {
 
     constructor(props) {
         super(props);
